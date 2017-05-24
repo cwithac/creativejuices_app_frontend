6718f60404557376e2b728d659500340703a25cf
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-24 16:30:08 -0400

    Authorization Testing

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,6 +1,7 @@
 angular.module('juicing-app').controller('loginController', ['$http', '$scope',
 function($http, $scope) {
 
+  //FOR TESTING ONLY
   // this.test = "Testing loginController"
   this.user = {};
 
@@ -18,6 +19,25 @@ function($http, $scope) {
     }).then(function(response){
       console.log(response);
       this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    }.bind(this));
+  };
+
+//FOR TESTING ONLY
+  this.getUsers = function() {
+    $http({
+      url: $scope.baseURL + 'users',
+      method: 'GET',
+      headers: {
+      'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+    }
+    }).then(function(response){
+      console.log(response);
+      if (response.data.status != 401) {
+        this.users = response.data;
+      } else {
+        this.error = "Unauthorized";
+      }
     }.bind(this));
   };
 
