bec91d86d2df3cb07394c6ad96bb2d6896da02fc
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-25 16:30:03 -0400

    Juices Index GET call working

--- public/js/controllers/juice-controller.js
+++ public/js/controllers/juice-controller.js
@@ -5,6 +5,21 @@ function($http, $scope) {
 //VARIABLES
 //------------------------------------------------------
 
-  
+//------------------------------------------------------
+//INDEX ROUTE FOR ALL JUICES
+//------------------------------------------------------
+
+  this.getAllJuices = function() {
+    $http({
+      method: 'GET',
+      url: $scope.baseURL + 'juices',
+    }).then(function(response){
+      console.log('all juices', response);
+      $scope.juices = response.data;
+      console.log('$scope.juices', $scope.juices);
+    }.bind(this));
+  };
+
+  this.getAllJuices();
 
 }]); // END juiceController
